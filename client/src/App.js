6ee3ae59c6c001e572d56104d30f9c1ee6bda07e
Mitchell Lev Merka <lev@merka.us>
2022-12-11 23:08:31 -0800

    redux store refactor, hook fix

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+import Counter from '../src/features/counter/Counter'
 
 const httpLink = createHttpLink({
   uri: '/graphql',
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import store from './app/store'
+import {Provider} from 'react-redux'
+import { Counter } from './features/counter/Counter';
 ReactDOM.render(
+<Provider store={store}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App>
+      <Counter/>
+    </App>
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
